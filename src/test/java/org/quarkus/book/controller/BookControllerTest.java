8ef15452197a64d8d9f29f8163efbd22e6a63ed1
renatoAlexsander <renato.alexsander13@gmail.com>
2020-06-23 07:40:40 -0300

    Add test for get book by id

--- src/test/java/org/quarkus/book/controller/BookControllerTest.java
+++ src/test/java/org/quarkus/book/controller/BookControllerTest.java
@@ -20,46 +20,76 @@ import static org.mockito.Mockito.when;
 @QuarkusTest
 class BookControllerTest {
 
+    private static final String BOOKS_ENDPOINT = "/api/books";
+
     @InjectMock
     BookService bookService;
 
     @Test
     public void shouldReturnAllBooks() {
         when(bookService.findAll())
-                .thenReturn(
-                        Response.ok(getBooks())
-                                .build());
+            .thenReturn(
+                Response.ok(getBooks())
+                    .build());
 
         given()
-                .when()
-                .get("/api/books")
-                .then()
-                .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("size()", is(2))
-                .body("[0].id", equalTo(1))
-                .body("[0].name", equalTo("JAVA 8"))
-                .body("[0].price", equalTo(100))
-                .body("[0].registerAt", notNullValue())
-                .body("[0].editedAt", nullValue());
+            .when()
+            .get(BOOKS_ENDPOINT)
+            .then()
+            .statusCode(200)
+            .contentType(ContentType.JSON)
+            .body("size()", is(2))
+            .body("[0].id", equalTo(1))
+            .body("[0].name", equalTo("JAVA 8"))
+            .body("[0].price", equalTo(100))
+            .body("[0].registerAt", notNullValue())
+            .body("[0].editedAt", nullValue());
 
         verify(bookService).findAll();
     }
 
     private Set<Book> getBooks() {
         return Set.of(
-                Book.builder()
-                        .id(1L)
-                        .name("JAVA 8")
-                        .price(BigDecimal.valueOf(100))
-                        .registerAt(LocalDateTime.now())
-                        .build(),
-                Book.builder()
-                        .id(2L)
-                        .name("JAVA 11")
-                        .price(BigDecimal.valueOf(99))
-                        .registerAt(LocalDateTime.now())
-                        .build()
+            Book.builder()
+                .id(1L)
+                .name("JAVA 8")
+                .price(BigDecimal.valueOf(100))
+                .registerAt(LocalDateTime.now())
+                .build(),
+            Book.builder()
+                .id(2L)
+                .name("JAVA 11")
+                .price(BigDecimal.valueOf(99))
+                .registerAt(LocalDateTime.now())
+                .build()
         );
+    }
+
+    @Test
+    public void shouldGetBookById() {
+        when(bookService.findById(1L))
+            .thenReturn(Response.ok(getBook()).build());
+
+        given()
+            .when()
+            .get(BOOKS_ENDPOINT + "/1")
+            .then()
+            .statusCode(200)
+            .contentType(ContentType.JSON)
+            .body("id", is(2))
+            .body("name", equalTo("JAVA 11"))
+            .body("price", is(99))
+            .body("registerAt", notNullValue());
+
+        verify(bookService).findById(1L);
+    }
+
+    private Book getBook() {
+        return Book.builder()
+            .id(2L)
+            .name("JAVA 11")
+            .price(BigDecimal.valueOf(99))
+            .registerAt(LocalDateTime.now())
+            .build();
     }
 }
